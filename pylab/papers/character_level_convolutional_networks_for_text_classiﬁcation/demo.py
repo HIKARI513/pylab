55b3ceb0eb4cf1523af1a1048dfffccb3a474e1a
howie6879 <xiaozizayang@gmail.com>
2019-01-03 09:08:15 +0800

    Update

--- pylab/papers/character_level_convolutional_networks_for_text_classiﬁcation/demo.py
+++ pylab/papers/character_level_convolutional_networks_for_text_classiﬁcation/demo.py
@@ -1,2 +1,18 @@
-import tensorflow as tf
+from keras import layers
+from keras import models
 
+model = models.Sequential()
+model.add(layers.Conv2D(256, (7, 7), activation='relu', input_shape=(1014, 69, 1)))
+model.add(layers.MaxPooling2D((3, 3)))
+model.add(layers.Conv2D(256, (7, 7), activation='relu'))
+model.add(layers.MaxPooling2D((3, 3)))
+model.add(layers.Conv2D(256, (3, 3), activation='relu'))
+model.add(layers.Conv2D(256, (3, 3), activation='relu'))
+model.add(layers.Conv2D(256, (3, 1), activation='relu'))
+model.add(layers.Conv2D(256, (3, 1), activation='relu'))
+model.add(layers.MaxPooling2D((3, 3)))
+
+# model.add(layers.Flatten())
+# model.add(layers.Dense(512, activation='relu'))
+# model.add(layers.Dense(2, activation='sigmoid'))
+model.summary()
