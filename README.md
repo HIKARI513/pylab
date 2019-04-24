8b3a71393b5812f68c71716597ca0a31fda7d7dd
howie6879 <xiaozizayang@gmail.com>
2019-04-24 10:36:39 +0800

    Update

--- README.md
+++ README.md
@@ -53,7 +53,7 @@
     - 官方代码：[Play-with-Linear-Algebra](https://github.com/liuyubobobo/Play-with-Linear-Algebra)
 
 - 极客时间：数据结构与算法之美
-	- 官方代码仓库：[algo](https://github.com/wangzheng0822/algo)
+	- 官方代码仓库：[algo](https://github.com/wangzheng0822/algo)：这里代码实现基本参照官方仓库
     - 笔记点[这里](pylab/lessons/data_structure_and_algorithms)
     - 具体笔记汇总点[这里](https://github.com/howie6879/pylab/issues/7)
 
---
+++ pylab/lessons/data_structure_and_algorithms/06_linkedlist/singly_linked_list.py
@@ -0,0 +1,114 @@
+# 实现单链表的插入、删除、查找等操作
+# 默认数据类型为int
+
+from typing import Optional, Union
+
+
+class Node:
+    """
+    链表结构的节点
+    """
+
+    def __init__(self, data: int, next=None):
+        """
+        初始化Node节点
+        :param data: 存储的数据
+        :param next: 后继指针
+        """
+        self.data = data
+        self.next = next
+
+
+class SinglyLinkedList:
+    def __init__(self):
+        """初始化单链表"""
+        self._head = None
+
+    def find_by_value(self, value: int) -> Optional[Node]:
+        """
+        根据值查找相应的节点
+        :param value:
+        :return:
+        """
+        p: Union[Node, None] = self._head
+        while p and p.data != value:
+            p = p.next
+        return p
+
+    def insert_value_to_head(self, value: int):
+        """
+        插入一个Node到头结点
+        :param value: 新头结点的值
+        :return:
+        """
+        new_node = Node(data=value)
+        self.insert_node_to_head(new_node)
+
+    def insert_node_to_head(self, new_node: Node):
+        """
+        构建头结点，让后继指针指向下一个节点
+        :param new_node: 新的头结点
+        :return:
+        """
+        if new_node:
+            new_node.next = self._head
+            self._head = new_node
+
+    def insert_value_before(self, node: Node, value: int):
+        """
+        在某个节点前插入节点
+        :param node: 目标节点
+        :param value: 待插入节点值
+        :return:
+        """
+        new_node = Node(value)
+
+    def insert_node_before(self, node: Node, new_node: Node):
+        """
+        新节点替代当前节点的位置，然后将新节点的后继指针指向当前节点
+        :param node: 当前节点
+        :param new_node: 新节点
+        :return:
+        """
+        if not self._head or not node or not new_node:
+            return
+
+        if self._head == node:
+            self.insert_node_to_head(new_node)
+            return
+
+        current = self._head
+        while current.next and current.next != node:
+            current = current.next
+
+    def print_all(self):
+        current: Union[Node, None] = self._head
+
+        if current:
+            print(f"{current.data}", end="")
+            current = current.next
+        while current:
+            print(f"->{current.data}", end="")
+            current = current.next
+        print("\n")
+
+    def __repr__(self) -> str:
+        nums = []
+        current: Union[Node, None] = self._head
+
+        while current:
+            nums.append(current.data)
+            current = current.next
+        if len(nums) > 0:
+            return "->".join(str(num) for num in nums)
+        else:
+            return ""
+
+
+if __name__ == "__main__":
+    l = SinglyLinkedList()
+    for i in range(15):
+        l.insert_value_to_head(i)
+    print(l)
+    node9 = l.find_by_value(9)
+    print(node9)
